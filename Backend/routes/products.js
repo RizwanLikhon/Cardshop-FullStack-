e7b2bca9fe67947e6c22f5dd43d613963a5c4509
A S M Rizwan Chowdhury <asmrizwan.chowdhury@mnsu.edu>
2025-12-27 16:30:36 -0600

    update

--- Backend/routes/products.js
+++ Backend/routes/products.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const router = express.Router();
-const path = require("path");
+const pool = require("../db");
 
-const products = require("../data/products.json");
+// GET all products OR filter by category
+router.get("/", async (req, res) => {
+  try {
+    const { category } = req.query;
 
-router.get("/", (req, res) => {
-  res.json(products);
+    let query = "SELECT * FROM products";
+    let values = [];
+
+    if (category) {
+      query += " WHERE category = $1";
+      values.push(category);
+    }
+
+    const result = await pool.query(query, values);
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server error" });
+  }
 });
 
 module.exports = router;
